7578da9c33d13e7669f723c0c196e88bafdcc657
SanazFeizi <sanazfeizikhankandi@gmail.com>
2023-08-09 23:43:20 -0400

    Add new course

--- RU_SummerCamp_GraphicDesign_2023/Primary.js
+++ RU_SummerCamp_GraphicDesign_2023/Primary.js
@@ -1,3 +1,5 @@
+
+/*
 function toggleActiveLink(link) {
     var links = document.querySelectorAll('.navigation a');
     links.forEach(function (item) {
@@ -5,3 +7,25 @@ function toggleActiveLink(link) {
     });
     link.classList.add('active');
 }
+*/
+
+
+let students = document.querySelectorAll("a");
+let container = document.querySelector('#container');
+
+for (let a of students){
+  a.onmouseenter = function(){
+    a.classList.add("hover")
+  }
+  a.onmouseleave = function(){
+    a.classList.remove("hover")
+  }
+}
+
+document.getElementById("shuffle").onclick=function(e){
+  e.preventDefault();
+  for (var i = container.children.length; i >= 0; i--) {
+      container.appendChild(container.children[Math.random() * i | 0]);
+  }
+}
+
